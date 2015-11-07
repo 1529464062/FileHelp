94d57f0e0ae645420a7a4a02d273903a62bc45a6
1529464062 <1529464062@qq.com>
2015-11-07 20:21:25 +0800

    完成Exists和CreateFile方法

---
+++ FileHelp/FileHelp/FileHelp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileHelp
+{
+    public class FileHelp
+    {
+        /// <summary>
+        /// 检测文件是否存在,存在返回true,不存在返回false
+        /// </summary>
+        /// <param name="FilePath">文件的路径</param>
+        /// <returns></returns>
+        public bool Exists(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 根据文件路径创建文件,若文件不存在则创建,若存在则覆盖,返回文件是否创建成功,成功则为true,不成功为false。
+        /// </summary>
+        /// <param name="FilePath">文件路径</param>
+        /// <returns></returns>
+        public bool CreateFile(string FilePath)
+        {
+            File.Create(FilePath);
+            return Exists(FilePath);
+        }
+    }
+}
